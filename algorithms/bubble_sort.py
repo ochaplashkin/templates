e37187b336c7f6a79f21f4225843be916614a43b
ochaplashkin <olegchaplashkin@gmail.com>
2017-05-17 22:09:48 +0400

    added time of  sorting 1000 elements

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,4 +1,6 @@
 
+import time
+
 def bubble_sort_up(array):
     "Algorithms bubble sort up O(n^2)"
     change = True
@@ -25,23 +27,26 @@ def bubble_sort_down(array):
 def unittest():
     print("running unittest...")
     try:
-        #test sort down
-        a = [x for x in range(0,10)]
-        bubble_sort_down(a)
-        k = 0
-        while k<(len(a)-1):
-            if a[k] < a[k+1]:
-                print("Test failed.Sort down didn't work.")
-                break
-            k +=1
-        #test sort up
-        k = 0
+        # test sort down
+        a = [x for x in range(1000, 0,-1)]
         bubble_sort_up(a)
+        k = 0
         while k < (len(a) - 1):
             if a[k] > a[k + 1]:
+                print("Test failed.Sort up didn't work.")
+                break
+            k += 1
+        # test sort up
+        k = 0
+        start = time.time()
+        bubble_sort_down(a)
+        end = time.time()
+        while k < (len(a) - 1):
+            if a[k] < a[k + 1]:
                 print("Test failed.Sort down didn't work.")
                 break
             k += 1
+        print('sorting 1000 elements(worst case):', str(end - start))
     except:
         print('Test failed. Other errors in code')
     print("finish...")
--- algorithms/shaker_sort.py
+++ algorithms/shaker_sort.py
@@ -1,3 +1,4 @@
+import time
 
 def cocktail_sort_up(array):
     "Algorithms cocktail(shaker) sort up O(n^2)"
@@ -13,9 +14,7 @@ def cocktail_sort_up(array):
             if array[i-1] > array[i]:
                 array[i],array[i-1] = array[i-1], array[i]
         left += 1
-
     return None
-
 
 def cocktail_sort_down(array):
     "Algorithms cocktail(shaker) sort down O(n^2)"
@@ -31,34 +30,34 @@ def cocktail_sort_down(array):
             if array[i - 1] < array[i]:
                 array[i], array[i - 1] = array[i - 1], array[i]
         left += 1
-
     return None
-
 
 def unittest():
     print("running unittest...")
     try:
-        # test sort down
-        a = [x for x in range(0, 10)]
-        cocktail_sort_down(a)
+        # test sort up
+        a = [x for x in range(1000, 0, -1)]
+        cocktail_sort_up(a)
         k = 0
+        while k < (len(a) - 1):
+            if a[k] > a[k + 1]:
+                print("Test failed.Sort up didn't work.")
+                break
+            k += 1
+        # test sort down
+        k = 0
+        start = time.time()
+        cocktail_sort_down(a)
+        end = time.time()
         while k < (len(a) - 1):
             if a[k] < a[k + 1]:
                 print("Test failed.Sort down didn't work.")
                 break
             k += 1
-        # test sort up
-        k = 0
-        cocktail_sort_up(a)
-        while k < (len(a) - 1):
-            if a[k] > a[k + 1]:
-                print("Test failed.Sort down didn't work.")
-                break
-            k += 1
+        print('sorting 1000 elements(worst case):', str(end - start))
     except:
         print('Test failed. Other errors in code')
     print("finish...")
-
 
 if __name__ == '__main__':
     unittest()
