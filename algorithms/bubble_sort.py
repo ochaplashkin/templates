53d650544f495654aa28edb8bd916fd58319e2b6
ochaplashkin <olegchaplashkin@gmail.com>
2017-05-17 21:23:28 +0400

    initial commit

---
+++ algorithms/bubble_sort.py
@@ -0,0 +1,50 @@
+
+def bubble_sort_up(array):
+    "Algorithms bubble sort up O(n^2)"
+    change = True
+    while change:
+        change = False
+        for i in range(len(array)-1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                change = True
+    return None
+
+
+def bubble_sort_down(array):
+    "Algorithms bubble sort down O(n^2)"
+    change = True
+    while change:
+        change = False
+        for i in range(len(array) - 1):
+            if array[i] < array[i + 1]:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                change = True
+    return None
+
+def unittest():
+    print("running unittest...")
+    try:
+        #test sort down
+        a = [x for x in range(0,10)]
+        bubble_sort_down(a)
+        k = 0
+        while k<(len(a)-1):
+            if a[k] < a[k+1]:
+                print("Test failed.Sort down didn't work.")
+                break
+            k +=1
+        #test sort up
+        k = 0
+        bubble_sort_up(a)
+        while k < (len(a) - 1):
+            if a[k] > a[k + 1]:
+                print("Test failed.Sort down didn't work.")
+                break
+            k += 1
+    except:
+        print('Test failed. Other errors in code')
+    print("finish...")
+
+if __name__ == '__main__':
+    unittest()
