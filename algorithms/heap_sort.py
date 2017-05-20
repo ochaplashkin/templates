af8a1b9d2846e707f61f0f5548f1ee78972f80b2
ochaplashkin <olegchaplashkin@gmail.com>
2017-05-20 19:52:40 +0400

    implemented correct algorithm heap sort

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,87 +1,44 @@
 import time
 
-class Binary_Heap():
-    def __init__(self):
-        self.size = None
-        pass
+#TODO:write comment in algorithm
 
-    def parent(self,i):
-        return int(i/2)
-    def left(self,i):
-        return 2*i
-    def right(self,i):
-        return 2*i+1
+def heapsort(s):
+    #it's correct algorithm
+    sl = len(s)
+    def swap(pi,ci):
+        if s[pi] < s[ci]:
+            s[pi], s[ci] = s[ci], s[pi]
+    def sift(pi,unsorted):
+        i_qt = lambda a,b: a if s[a]>s[b] else b
+        while pi*2 + 2 < unsorted:
+            qtci = i_qt(pi*2+1,pi*2+2)
+            swap(pi,qtci)
+            pi = qtci
+    for i in  range(int((sl/2))-1,-1,-1):
+        sift(i,sl)
 
-    def heapify(self,array,i):
-        self.size = len(array)-1
-        l = self.left(i)
-        r= self.right(i)
-        if (l <= self.size) and (array[l] > array[i]):
-            largest = l
-        else:
-            largest = i
-        if (r <= self.size) and (array[r] > array[largest]):
-            largest = r
-        if largest != i:
-            array[i], array[largest] = array[largest],array[i]
-            self.heapify(array,largest)
+    for i in range(sl-1,0,-1):
+        swap(i,0)
+        sift(0,i)
 
-    def build_heap(self,array):
-        self.size = len(array)-1
-        for i in range(int(len(array)/2),1,-1):
-            self.heapify(array,i)
-        pass
-    def heap_sort(self,array):
-        #first variant with class heap
-        #it's dont'work...
-        self.build_heap(array)
-        size = len(array)-1
-        for i in range(len(array)-1,2,-1):
-            array[1],array[i] = array[i], array[1]
-            self.size -= 1
-            self.heapify(array,1)
-        pass
-    def extract_max(self):
-        pass
-    def insert(self):
-        pass
-
-def sort(array):
-    #second variant without heap class
-    #it's don't work...
-    #TODO: implement correct variant
-    length = len(array) - 1
-    leastParent = int(length / 2)
-    print(length,leastParent)
-    for i in range(leastParent,0,-1):
-        moveDown(array,i,length)
-
-    for i in range(length,0,-1):
-        if array[0] > array[i]:
-            array[0],array[i] = array[i],array[0]
-            moveDown(array,0,i-1)
-
-def moveDown(array,first,last):
-    largest = 2*first + 1
-    while largest <= last:
-        if (largest < last) and (array[largest] < array[largest + 1]):
-            largest +=1
-        if array[largest] > array[first]:
-            largest,first = first,largest
-            first = largest
-            largest = 2*first + 1
-        else:
-            return [0]
 
 def unittest():
-    print('unittest...')
-    heap = Binary_Heap()
-    a = [8,5,3,1,9,6,0,7,4,2,5]
-    print('Array',a,len(a))
-    sort(a)
-    print('Result',a)
-
-
+    print("running unittest...")
+    try:
+        a = [x for x in range(1000, 0, -1)]
+        start = time.time()
+        heapsort(a)
+        end = time.time()
+        k = 0
+        while k < (len(a) - 1):
+            if a[k] > a[k + 1]:
+                print("Test failed.Sort up didn't work.")
+                break
+            k += 1
+        print('sorting 1000 elements(worst case):', str(end - start))
+    except:
+        print('Test failed. Other errors in code')
+    print("finish...")
 
 if __name__ == '__main__':
     unittest()
