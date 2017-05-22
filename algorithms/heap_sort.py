fe1d483cbc2567e4a81886055dc93d8f90097aa4
ochaplashkin <olegchaplashkin@gmail.com>
2017-05-22 23:13:58 +0400

    added "step by step" comments

--- algorithms/heap_sort.py
+++ algorithms/heap_sort.py
@@ -1,25 +1,34 @@
 import time
 
-#TODO:write comment in algorithm
-
 def heapsort(s):
-    #it's correct algorithm
+    "Algorithm heap sort"
     sl = len(s)
+
     def swap(pi,ci):
+        "Функция обмена в массиве'"
         if s[pi] < s[ci]:
             s[pi], s[ci] = s[ci], s[pi]
-    def sift(pi,unsorted):
+
+    def siftDown(pi,unsorted):
+        """Функция пробегает по пирамиде восстанавливая ее
+        также используется для начального создания пирамиды.
+        (пробежит по всем потомкам и
+         найдет нужное место для следующего элемента)
+        """
         i_qt = lambda a,b: a if s[a]>s[b] else b
         while pi*2 + 2 < unsorted:
-            qtci = i_qt(pi*2+1,pi*2+2)
-            swap(pi,qtci)
+            qtci = i_qt(pi*2+1,pi*2+2)#выбор наибольшего(левый или правый потомок)
+            swap(pi,qtci) #обмен
             pi = qtci
-    for i in  range(int((sl/2))-1,-1,-1):
-        sift(i,sl)
 
+    #собираем пирамиду из массива
+    for i in  range(int((sl/2))-1,-1,-1):
+        siftDown(i,sl)
+    #пирамида собрана
+    #сортировка
     for i in range(sl-1,0,-1):
-        swap(i,0)
-        sift(0,i)
+        swap(i,0)#перемещаем верхушку в начало отсортированного списка
+        siftDown(0,i)#находим нужное место для нового элемента
 
 
 def unittest():
